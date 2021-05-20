3e0dcdf7d6cc6a24531af38eaff31bdebe62bb55
katarzynastacel <katarzyna.stacel92@gmail.com>
2021-05-20 22:59:07 +0200

    initial

---
+++ DemoTSSP/ClientApp/src/App.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
+
+import "./custom.css";
+
+interface IAppProps {
+  className?: string;
+}
+const App: React.FunctionComponent<IAppProps> = (props: IAppProps) => {
+  return (
+    <Layout>
+      <Route exact path="/" component={Home} />
+      <Route path="/counter" component={Counter} />
+      <Route path="/fetch-data" component={FetchData} />
+    </Layout>
+  );
+};
